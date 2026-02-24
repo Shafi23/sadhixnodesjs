4a67c75e448a1af48ac30ae1f64d07a65599759b
Shafi23 <105143502+Shafi23@users.noreply.github.com>
2026-02-24 12:10:30 +0530

    Fix formatting and minor text adjustment in app.js

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome to my app Server. \n');
+  res.end('Welcome to my app Server.. \n');
 });
 
 // Start the server
 server.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}/`);
 });
+
 
 
